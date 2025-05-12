7b7a5536580531332cbbd1e48ed49bf88c9bd975
Warlych <raccoon.warly@gmail.com>
2025-05-12 21:44:41 +0300

    add railway sections init migration

---
+++ src/Microservices/RailwaySections/RailwaySections.Persistence/Migrations/20250512184349_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RailwaySections.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "railway_sections",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    parameters_railway_code = table.Column<string>(type: "text", nullable: false),
+                    parameters_unified_network_marking = table.Column<string>(type: "text", nullable: false),
+                    title_full_name = table.Column<string>(type: "text", nullable: false),
+                    title_mnemonic = table.Column<string>(type: "text", nullable: true),
+                    title_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_railway_sections", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "railway_sections");
+        }
+    }
+}
